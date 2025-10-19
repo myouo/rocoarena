ea7bf128f82213a85d1129d6ebc212a86a8d33a2
myouo <chizurumyo@gmail.com>
2025-10-19 23:59:24 +0800

    更新Species

--- cxx/arena/entity/Species.h
+++ cxx/arena/entity/Species.h
@@ -27,7 +27,7 @@ public:
     using Ptr = std::shared_ptr<const Species>;
 
     static Ptr createFromJson(const nlohmann::json& j);
-
+    
     const int id() const noexcept {return id_;}
     const std::string& name() const noexcept {return name_;}
     const std::array<AttrType, 2>& attrs() const noexcept {return attrs_;}
