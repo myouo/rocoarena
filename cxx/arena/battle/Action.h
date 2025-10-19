c87764e0588e501a84c39adde5fffdf6da0b6baa
myouo <chizurumyo@gmail.com>
2025-10-19 23:59:56 +0800

    新增Action战斗动作类，完善中

---
+++ cxx/arena/battle/Action.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Forward.h"
+
+enum class ActionType {
+    Skill,
+    Switch,
+    Potion,
+    Flee
+};
+
+class Action {
+public:
+    Action(ActionType type) : type_(type) {};
+    virtual ~Action() = default;
+
+    ActionType getType() const {return type_;}
+    
+    virtual void execute(BattleSystem& battle, Pet& self, Pet& opponent) = 0;
+private:
+    ActionType type_;
+};
