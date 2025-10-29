dcf5059f7458ae42da3fe2c48e56986b37d3b5de
myouo <chizurumyo@gmail.com>
2025-10-30 02:01:04 +0800

    🐾 set lua test cases

---
+++ cxx/arena/data/skills/test.lua
@@ -0,0 +1,7 @@
+print("lua load success!")
+
+function add(a, b)
+    return a + b
+end
+
+x = 2212
--- cxx/arena/utils/Scripter/Scripter.h
+++ cxx/arena/utils/Scripter/Scripter.h
@@ -1,29 +1,55 @@
 #pragma once
 
+/*
+sudo apt install lua5.3 lublua5.3-dev
+*/
 #include <sol/sol.hpp>
 #include <string>
 #include <iostream>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define RED_PRINT (std::cout << ANSI_COLOR_RED << "[Scripter] " << ANSI_COLOR_RESET)
+#define RED_CERR (std::cerr << ANSI_COLOR_RED << "[Scripter] " << ANSI_COLOR_RESET)
 
 class Scripter {
 public:
-    // 构造函数
+    // construct
     Scripter();
-    // 执行 Lua 脚本
+    // run Lua script
     bool runScript(const std::string& filename);
-    // 执行 Lua 字符串脚本
+    // run Lua string
     bool runString(const std::string& code);
     // get Lua var
     template <typename T>
     T get(const std::string& name) {
         return lua[name];
-    }
+    };
     // call Lua function
     template <typename Ret, typename... Args> 
-    Ret call(const std::string& funcName, Args... args); 
+    Ret call(const std::string& funcName, Args... args) {
+        sol::function func = lua[funcName];
+        if(!func.valid()) {
+            throw std::runtime_error("[Scripter] Function '" + funcName + "' not found in Lua.");
+        }
+        return func(args...);
+   
+    }
+
     //register cpp functions for Lua
     template <typename Func>
-    void registerFunction(const std::string&& name, Func f);
+    void registerFunction(const std::string& name, Func f) {
+        lua.set_function(name, f);
+    }
+
+    template <typename T>
+    void set(const std::string& name, T val) {
+        lua[name] = val;
+    }
 
 private:
     sol::state lua;
