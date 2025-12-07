f80c8123f12ea80a56fff3346b8a4f198ff5ac49
myouo <chizurumyo@gmail.com>
2025-12-07 20:16:21 +0800

    调整测试文件，新增 ignore，取消上传 build...

--- arena/tests/core/testJSON.cpp
+++ arena/tests/core/testJSON.cpp
@@ -7,9 +7,5 @@ int main() {
     std::string any_ = loader.get<std::string>("title", "DEFAULT_PRINT");
     std::string any_1 = loader.get<std::string>("author", "DEFAULT_PRINT");
     int any_2 = loader.get<int>("published_year", 0);
-
-    GREEN_PRINT << any_ << "\n";
-    GREEN_PRINT << any_1 << "\n";
-    GREEN_PRINT << any_2 << "\n";
     return 0;
 }
--- arena/tests/core/testLua.cpp
+++ arena/tests/core/testLua.cpp
@@ -12,13 +12,10 @@ int main() {
     ss << DIR_CURRENT << "/testcases/test.lua";
     std::string dir = ss.str();
     
-    RED_PRINT << dir << "\n";
     scr.runScript(dir);
     scr.runString("printf(1111)");
     int res = scr.call<int>("add", 10, 20);
-    RED_PRINT << res << "\n";
     int x = scr.get<int>("x");
-    RED_PRINT << x << "\n";
     scr.registerFunction("calc", calc);
     scr.runString(R"(
         print(calc(20, 34))
