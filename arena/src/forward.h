dcd861728310b46d4871ccfe5b7fe183adb55d27
myouo <chizurumyo@gmail.com>
2025-12-18 00:36:37 +0800

    🙄 毫无意义的转发类以及项目结构调整

---
+++ arena/src/forward.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// class forward
+
+class BattleSystem;
+class Action;
+class Buff;
+
+// entity
+class Pet;
+class AttrChart;
+class Player;
+
+// skills
+class SkillBase;
+class SkillPool;
+class SkillRegistry;
+class SkillAction;
